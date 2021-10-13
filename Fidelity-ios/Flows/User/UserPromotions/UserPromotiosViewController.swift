8beebe91f4e092be08a97592f6c70a95a81adc8d
Matheus Homrich <37409567+matheushomrich@users.noreply.github.com>
2021-10-13 16:49:49 -0300

    wip - userPromotionView

---
+++ Fidelity-ios/Flows/User/UserPromotions/UserPromotiosViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserPromotiosView.swift
+//  Fidelity-ios
+//
+//  Created by Matheus Homrich on 13/10/21.
+//
+
+import Foundation
+import UIKit
+
+
+class UserPromotionsViewController: UIViewController {
+    
+    private let topView: UIView = {
+        
+    }
+    private let topViewImage: UIImageView
+    private let topViewUserName: UILabel
+    private let topViewUserPointsDescription: UILabel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
