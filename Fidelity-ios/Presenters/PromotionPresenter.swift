e4c92a121509b76b0b104cb5f906206157b6e5e8
PePacheco <pedro_pacheco98@outlook.com>
2021-10-13 14:59:46 -0300

    Adding the promotion presenter

--- Fidelity-ios/Presenters/PromotionPresenter.swift
+++ Fidelity-ios/Presenters/PromotionPresenter.swift
@@ -6,3 +6,86 @@
 //
 
 import Foundation
+import UIKit
+
+protocol PromotionPresenterDelegate: AnyObject {
+    func fetched(promotions: [Promotion])
+    func created(promotion: Promotion)
+}
+
+extension PromotionPresenterDelegate {
+    func fetched(promotions: [Promotion]) {}
+    func created(promotion: Promotion) {}
+}
+
+class PromotionPresenter {
+    
+    weak var view: (PromotionPresenterDelegate & UIViewController)?
+    
+    init() {}
+    
+    func createPromotion(code: String?, name: String?, award: String?, ticketType: String?, winAmount: Int?, start: Date?, end: Date?) {
+        guard let code = code,
+              let name = name,
+              let award = award,
+              let ticketType = ticketType,
+              let winAmount = winAmount,
+              let start = start,
+              let end = end else {
+                  view?.presentAlert(message: "Você precisa preencher todos os campos para criar uma promoção", title: "Oops!")
+                  return
+              }
+        
+        if winAmount < 1 {
+            view?.presentAlert(message: "O total para retirar o prêmio deve ser maior que zero")
+        }
+        
+        if end < start {
+            view?.presentAlert(message: "A data do fim da promoção não pode ser antes do início ")
+        }
+        
+        let body: [String: AnyHashable] = [
+            "code": code,
+            "name": name,
+            "award": award,
+            "ticket_type": ticketType,
+            "win_ticket_amount": winAmount,
+            "start": start,
+            "end": end
+        ]
+        
+        view?.presentLoadingScreen()
+        WebService.post(path: "", body: body, type: Promotion.self) {[weak self] result in
+            guard let self = self else {
+                self?.view?.dismiss(animated: true, completion: nil)
+                return
+            }
+            switch result {
+            case .success(let promotion):
+                self.view?.created(promotion: promotion)
+                self.view?.dismiss(animated: true, completion: nil)
+            case .failure(let error):
+                print(error)
+                self.view?.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func fetchPromotions() {
+        view?.presentLoadingScreen()
+        WebService.get(path: "", type: [Promotion].self) {[weak self] result in
+            guard let self = self else {
+                self?.view?.dismiss(animated: true, completion: nil)
+                return
+            }
+            switch result {
+            case .success(let promotions):
+                self.view?.fetched(promotions: promotions)
+                self.view?.dismiss(animated: true, completion: nil)
+            case .failure(let error):
+                self.view?.dismiss(animated: true, completion: nil)
+                print(error)
+            }
+        }
+    }
+}
---
+++ Fidelity-ios/Resources/Extensions/ControllerExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  ControllerExtensions.swift
+//  Fidelity-ios
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 13/10/21.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentLoadingScreen() {
+        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating();
+        
+        alert.view.addSubview(loadingIndicator)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func presentAlert(message: String, title: String = "Oops") {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
