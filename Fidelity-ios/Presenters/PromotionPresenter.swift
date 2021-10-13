6a30bc3697cbb6a1a67d722df70d4370c01f494a
PePacheco <pedro_pacheco98@outlook.com>
2021-10-13 14:03:04 -0300

    Adding promotion view model and presenter

---
+++ Fidelity-ios/Presenters/PromotionPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  PromotionPresenter.swift
+//  Fidelity-ios
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 13/10/21.
+//
+
+import Foundation
---
+++ Fidelity-ios/View Models/PromotionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PromotionViewModel.swift
+//  Fidelity-ios
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 13/10/21.
+//
+
+import Foundation
