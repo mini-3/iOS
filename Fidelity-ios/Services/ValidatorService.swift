eeafc0e3537460c6ca1c13a93cf49a690aa32b26
artur <arturluisoliveira@gmail.com>
2021-10-19 21:43:07 -0300

    mocked validation service

---
+++ Fidelity-ios/Services/ValidatorService.swift
@@ -0,0 +1,48 @@
+//
+//  ValidatorService.swift
+//  Fidelity-ios
+//
+//  Created by Artur Luis on 19/10/21.
+//
+
+import Foundation
+
+enum ValidatorDataTypes {
+    case CPF
+    case email
+    case ISOString
+    case password
+    case CNPJ
+}
+
+struct ValidatorService {
+    static func validate(value: String, type: ValidatorDataTypes) -> Bool {
+        [
+            ValidatorDataTypes.CPF: validateCpf(value),
+            ValidatorDataTypes.email: validateEmail(value),
+            ValidatorDataTypes.ISOString: validateISOString(value),
+            ValidatorDataTypes.password: validatePassword(value),
+            ValidatorDataTypes.CNPJ: validateCNPJ(value)
+        ][type] ?? false
+    }
+    
+    static private func validateCpf(_ cpf: String) -> Bool {
+        return true
+    }
+    
+    static private func validateEmail(_ email: String)-> Bool {
+        return true
+    }
+    
+    static private func validatePassword(_ password: String) -> Bool{
+        return true
+    }
+    
+    static private func validateCNPJ(_ cnpj: String) -> Bool {
+        return true
+    }
+    
+    static private func validateISOString(_ isoString: String) -> Bool {
+        return true
+    }
+}
