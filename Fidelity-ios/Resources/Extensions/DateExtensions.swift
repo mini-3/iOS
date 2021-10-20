a361a201a6df575e31d453196a61daef0ee1d04f
artur <arturluisoliveira@gmail.com>
2021-10-19 21:11:26 -0300

    to iso string extension

--- Fidelity-ios/Resources/Extensions/DateExtensions.swift
+++ Fidelity-ios/Resources/Extensions/DateExtensions.swift
@@ -11,4 +11,12 @@ extension Date {
     func hourAfter(n: Int) -> Date {
         return Calendar.current.date(byAdding: .hour, value: n, to: self)!
     }
+    
+    func toISOString() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        return formatter.string(from: self)
+    }
 }
