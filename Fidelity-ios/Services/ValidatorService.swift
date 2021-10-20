955a396e8116369fbf3a8cdde6e1fca5fbdde3a7
PePacheco <pedro_pacheco98@outlook.com>
2021-10-20 15:33:01 -0300

    Adding cpf validator, but not using it

--- Fidelity-ios/Services/ValidatorService.swift
+++ Fidelity-ios/Services/ValidatorService.swift
@@ -31,9 +31,7 @@ struct ValidatorService {
     }
     
     static private func validateEmail(_ email: String)-> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
+        return email.isValidEmail()
     }
     
     static private func validatePassword(_ password: String) -> Bool{
