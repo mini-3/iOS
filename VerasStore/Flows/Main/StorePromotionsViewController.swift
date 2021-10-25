afa0128b52c5b2a2b53e2f77c13f1970c7872947
Santiago <santiago.dgonzaga@hotmail.com>
2021-10-25 16:07:15 -0300

    create view controller and basic structures

---
+++ VerasStore/Flows/Main/StorePromotionsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StorePromotionsViewController.swift
+//  VerasStore
+//
+//  Created by Santiago del Castillo Gonzaga on 25/10/21.
+//
+
+import Foundation
+import UIKit
+
+class StorePromotionsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureUI()
+        self.configureSubViews()
+        self.configureConstraints()
+    }
+    
+    private func configureUI() {
+        
+    }
+    
+    private func configureSubViews() {
+        
+    }
+    
+    private func configureConstraints() {
+        
+    }
+}
