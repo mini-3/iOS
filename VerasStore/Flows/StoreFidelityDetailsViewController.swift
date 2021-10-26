921f83831d215339516d9f7ac3dfecbcb8395af4
PePacheco <pedro_pacheco98@outlook.com>
2021-10-26 15:23:50 -0300

    Adjusting typo

--- VerasStore/Flows/StoreFidelityDetailsViewController.swift
+++ VerasStore/Flows/StoreFidelityDetailsViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class StoreFidelityDetailsViewController: ViewController {
+class StoreFidelityDetailsViewController: UIViewController {
     
     var promotion: PromotionViewModel?
     
