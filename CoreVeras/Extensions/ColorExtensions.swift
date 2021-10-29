7004cfa9d075988371cd32c2bf59c444fd23b6d7
PePacheco <pedro_pacheco98@outlook.com>
2021-10-29 16:00:36 -0300

    Adding scroll view to the create/update promotion view controller

--- CoreVeras/Extensions/ColorExtensions.swift
+++ CoreVeras/Extensions/ColorExtensions.swift
@@ -48,6 +48,30 @@ extension UIColor {
 }
 
 extension UIColor {
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
+extension UIColor {
     
     func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
         return self.adjust(by: abs(percentage) )
