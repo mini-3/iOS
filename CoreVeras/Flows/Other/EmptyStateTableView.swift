b5435df177e8cc1afdb8987eea24514e5021ce23
Santiago <santiago.dgonzaga@hotmail.com>
2021-11-04 14:29:50 -0300

    create tableView Empty State

---
+++ CoreVeras/Flows/Other/EmptyStateTableView.swift
@@ -0,0 +1,75 @@
+//
+//  EmptyStateTableView.swift
+//  Fidelity-ios
+//
+//  Created by Santiago del Castillo Gonzaga on 04/11/21.
+//
+
+import UIKit
+
+public class EmptyStateTableView: UIView {
+    
+    init(image: UIImage, label: String) {
+        super.init(frame: .zero)
+        self.imageView.image = image
+        self.titleLabel.text = label
+        
+        addSubviews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private var verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    
+    private var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .white
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.numberOfLines = 1
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private func addSubviews() {
+        self.addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(imageView)
+        verticalStackView.addArrangedSubview(titleLabel)
+    }
+    
+    private func addConstraints() {
+        
+        let verticalStackViewConstraints = [
+            verticalStackView.centerXAnchor.constraint(equalTo: superview?.centerXAnchor ?? self.centerXAnchor),
+            verticalStackView.centerYAnchor.constraint(equalTo: superview?.centerYAnchor ?? self.centerYAnchor)
+        ]
+        
+        let imageViewConstraints = [
+            imageView.heightAnchor.constraint(equalToConstant: 150),
+            imageView.widthAnchor.constraint(equalToConstant: 150)
+        ]
+        
+        NSLayoutConstraint.activate(verticalStackViewConstraints)
+        NSLayoutConstraint.activate(imageViewConstraints)
+    }
+}
